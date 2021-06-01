f3d8b298fe4031cdb78a50dd93a60b7189b2a034
Benjscho <benjamin.j.schofield+coding@gmail.com>
2021-06-01 13:47:44 +0100

    Print board working
    Add print board function, also add skeletons for piece classes. Next to
    do is to flesh out the method for inputing pieces, get a functioning
    piece class/struct and work out the drop piece function.

--- py-src/piece.py
+++ py-src/piece.py
@@ -38,8 +38,23 @@ brick_masks_inv = np.array([
   0x0001  # XXXXXXXXXXXXXXX. */
 ], np.uint16)
 
+class PieceOrientation():
+    def __init__(self, height, width, shape, nb_full_cells):
+        self.height = height
+        self.width = width
+        
+
+
+class Piece():
+    def __init__(self, nb_orientations, orientations):
+        self.nb_orientations
+        self.orientations = orientations
+        
+
+
 class Board():
-    def __init__(self, width, height, nb_pieces, pieces)
+    def __init__(self, width, height, nb_pieces, pieces):
+        if width > 14 or width < 5: raise ValueError(f"Width must be between 5 and 14, value given: {width}")
         self.width = width
         self.height = height
         self.pieces = pieces
@@ -48,18 +63,26 @@ class Board():
         self.empty_row = brick_masks[0] | brick_masks[width+1]
         for i in range(width+2, 16):
             self.empty_row |= brick_masks[i]
-        self.board = np.array([self.empty_row]*self.height], np.uint16)
 
         self.max_piece_height = 0
-        for i in range(nb_pieces):
-            for j in range(pieces[i].nb_orientations):
-                self.max_piece_height = max(self.max_piece_height, pieces[i].orientations[j].height)
+        #for i in range(nb_pieces):
+        #    for j in range(pieces[i].nb_orientations):
+        #        self.max_piece_height = max(self.max_piece_height, pieces[i].orientations[j].height)
+        self.extended_height = height + self.max_piece_height
+
+        self.board = np.array([self.empty_row]*self.extended_height, np.uint16)
+        self.wall_height = 0
+
 
     def reset(self):
-        self.board = np.array([self.empty_row]*self.height], np.uint16)
+        self.board = np.array([self.empty_row]*self.extended_height, np.uint16)
+        self.wall_height = 0
     
     def drop_piece(self, piece, action, cancellable):
-        pass
+        if cancellable:
+            self.backup_board = self.board
+            self.previous_wall_height = self.wall_height
+
 
     def cancel_last_move(self):
         pass
@@ -68,4 +91,20 @@ class Board():
         pass
 
     def print_board(self):
-        pass
+        for i in range(self.wall_height, self.height - 1, -1):
+            print(" ", end="")
+            for j in range(1, self.width +1):
+                if self.board[i] & brick_masks[j]:
+                    print("X",end="")
+                else:
+                    print(" ",end="")
+            print("")
+        
+        for i in range(self.height - 1, -1, -1):
+            print("|", end="")
+            for j in range(1, self.width+1):
+                if self.board[i] & brick_masks[j]:
+                    print("X", end="")
+                else:
+                    print(".",end="")
+            print("|")
